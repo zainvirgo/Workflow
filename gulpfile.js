a779d75c12a0d1739fb1671ea209a46ea447aee8
Zaheen <zaheen.ahmed@springbokagency.com>
2017-01-09 22:50:24 +0100

    gulp dependency injection

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reloade = browserSync.reload;
+
+var SOURCEPATH = {
+    sassSource: 'src/scss/*.scss'
+}
+
+var APPPATH = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
 
 gulp.task('sass', function(){
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(SOURCEPATH.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default', ['sass']);
+
+gulp.task('serve', ['sass'], function(){
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+    server: {
+    baseDir : APPPATH.root
+       }
+    })
+});
+
+gulp.task('watch', ['serve', 'sass'], function(){
+    gulp.watch([SOURCEPATH.sassSource], ['sass']);
+});
+
+gulp.task('default', ['watch']);

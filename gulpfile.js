3cb1223b556ae1f066cf71380f3ba5dcdcaf9272
Zaheen <zaheen.ahmed@springbokagency.com>
2017-01-10 22:36:48 +0100

    copy task in workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reloade = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATH = {
-    sassSource: 'src/scss/*.scss'
+    sassSource : 'src/scss/*.scss',
+    htmlSource : 'src/*.html'
 }
 
 var APPPATH = {
@@ -15,10 +17,15 @@ var APPPATH = {
 
 gulp.task('sass', function(){
     return gulp.src(SOURCEPATH.sassSource)
+    .pipe(autoprefixer())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('copy', function() {
+    gulp.src(SOURCEPATH.htmlSource)
+        .pipe(gulp.dest(APPPATH.root))
+});
 
 gulp.task('serve', ['sass'], function(){
     browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
@@ -28,8 +35,9 @@ gulp.task('serve', ['sass'], function(){
     })
 });
 
-gulp.task('watch', ['serve', 'sass'], function(){
+gulp.task('watch', ['serve', 'sass', 'copy'], function(){
     gulp.watch([SOURCEPATH.sassSource], ['sass']);
+    gulp.watch([SOURCEPATH.htmlSource], ['copy'])
 });
 
 gulp.task('default', ['watch']);
